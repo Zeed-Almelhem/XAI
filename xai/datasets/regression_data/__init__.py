ffaa583a2c0de9cd4f83707aea1a8faeb7ea10b1
Zeed-Almelhem <zeed.almelhem@zeed-almelhem.com>
2023-09-08 19:31:29 -0700

    two new functions have been added to the datasets/regression_data/__init__.py file. The load_admission_prediction_data function loads the Admission Prediction dataset, and the load_bike_sharing_demand_data function loads the Bike Sharing Demand dataset. These functions provide convenient ways to access the datasets for regression analysis. Users can now easily load these datasets into their projects by calling these functions.

---
+++ xai/datasets/regression_data/__init__.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def load_admission_prediction_data():
+    """
+    Load the Admission Prediction dataset from the regression_data directory.
+    
+    Returns:
+    DataFrame: A Pandas DataFrame containing the Admission Prediction dataset.
+    """
+    data_path = r"Z:\Open Source\XAI\xai\datasets\regression_data\Admission_Prediction.csv"
+    df = pd.read_csv(data_path)
+    return df
+
+def load_bike_sharing_demand_data():
+    """
+    Load the Bike Sharing Demand dataset from the regression_data directory.
+    
+    Returns:
+    DataFrame: A Pandas DataFrame containing the Bike Sharing Demand dataset.
+    """
+    data_path = r"Z:\Open Source\XAI\xai\datasets\regression_data\bike_sharing_demand.csv"
+    df = pd.read_csv(data_path)
+    return df
