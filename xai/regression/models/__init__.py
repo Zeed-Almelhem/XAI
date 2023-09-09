85027e26439cd5735287abdb983f3fcbbcd8ae4a
Zeed-Almelhem <zeed.almelhem@zeed-almelhem.com>
2023-09-09 12:57:18 -0700

    Updated the library to version  0.0.4 after thorough debugging and addressing various issues and errors. This release aims to provide a more stable and improved experience for users, ensuring that the XAI library functions smoothly and efficiently.

--- xai/regression/models/__init__.py
+++ xai/regression/models/__init__.py
@@ -183,13 +183,13 @@ def load_admission_regression_data_and_model():
     """
     # Load data
     #models\admission_prediction_X_test.csv
-    X_train = pd.read_csv(r'./admission_prediction_X_train.csv')
-    y_train = pd.read_csv(r'./admission_prediction_y_train.csv')
-    X_test = pd.read_csv(r'./admission_prediction_X_test.csv')
-    y_test = pd.read_csv(r'./admission_prediction_y_test.csv')
+    X_train = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/admission_prediction_X_train.csv')
+    y_train = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/admission_prediction_y_train.csv')
+    X_test = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/admission_prediction_X_test.csv')
+    y_test = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/admission_prediction_y_test.csv')
 
     # Load the Linear Regression model
-    model = joblib.load('./linear_regression_model.pkl')
+    model = joblib.load('linear_regression_model.pkl')
 
     return X_train, y_train, X_test, y_test, model
 
@@ -205,13 +205,13 @@ def load_lasso_regression_data_and_model():
         model: Lasso Regression model.
     """
     # Load data
-    X_train = pd.read_csv(r'./admission_prediction_X_train.csv')
-    y_train = pd.read_csv(r'./admission_prediction_y_train.csv')
-    X_test = pd.read_csv(r'./admission_prediction_X_test.csv')
-    y_test = pd.read_csv(r'./admission_prediction_y_test.csv')
+    X_train = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/admission_prediction_X_train.csv')
+    y_train = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/admission_prediction_y_train.csv')
+    X_test = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/admission_prediction_X_test.csv')
+    y_test = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/admission_prediction_y_test.csv')
 
     # Load the Lasso Regression model
-    model = joblib.load('./lasso_regression_model.pkl')
+    model = joblib.load('lasso_regression_model.pkl')
 
     return X_train, y_train, X_test, y_test, model
 
@@ -227,13 +227,13 @@ def load_random_forest_data_and_model():
         model: Random Forest model.
     """
     # Load data
-    X_train = pd.read_csv(r'./bike_sharing_demand_X_train.csv')
-    y_train = pd.read_csv(r'./bike_sharing_demand_y_train.csv')
-    X_test = pd.read_csv(r'./bike_sharing_demand_X_test.csv')
-    y_test = pd.read_csv(r'./bike_sharing_demand_y_test.csv')
+    X_train = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/bike_sharing_demand_X_train.csv')
+    y_train = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/bike_sharing_demand_y_train.csv')
+    X_test = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/bike_sharing_demand_X_test.csv')
+    y_test = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/bike_sharing_demand_y_test.csv')
 
     # Load the Random Forest model
-    model = joblib.load('./random_forest_model.pkl')
+    model = joblib.load('random_forest_model.pkl')
 
     return X_train, y_train, X_test, y_test, model
 
@@ -249,12 +249,12 @@ def load_support_vector_data_and_model():
         model: Support Vector model.
     """
     # Load data
-    X_train = pd.read_csv(r'./bike_sharing_demand_X_train.csv')
-    y_train = pd.read_csv(r'./bike_sharing_demand_y_train.csv')
-    X_test = pd.read_csv(r'./bike_sharing_demand_X_test.csv')
-    y_test = pd.read_csv(r'./bike_sharing_demand_y_test.csv')
+    X_train = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/bike_sharing_demand_X_train.csv')
+    y_train = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/bike_sharing_demand_y_train.csv')
+    X_test = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/bike_sharing_demand_X_test.csv')
+    y_test = pd.read_csv(r'https://raw.githubusercontent.com/Zeed-Almelhem/XAI/main/xai/regression/models/bike_sharing_demand_y_test.csv')
     # Load the Support Vector model
-    model = joblib.load(r'./support_vector_model.pkl')
+    model = joblib.load(r'support_vector_model.pkl')
 
     return X_train, y_train, X_test, y_test, model
 
